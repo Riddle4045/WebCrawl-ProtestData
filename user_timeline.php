c5893a24b044cbc48fc0e9b6250eb347359b6927
Ishan <ipatwa@ufl.edu>
2014-06-02 18:47:24 -0400

    cleaning user_timeline

--- user_timeline.php
+++ user_timeline.php
@@ -41,13 +41,15 @@ $new_getfield = "";
 $GLOBALS['new_max_id'] = $max_id + 1;
 $GLOBALS['old_max_id'] = $max_id;
 
-//$users = array(1 => '@CamThompsonWNEW', 2 => '@Organizerx', 3 => '@Cool_Revolution', 4 => '@johnzangas', 5 => '@rousseau_ist', 6 => '@Gen_Knoxx', 7 => '@jamesFTinternet', 8 => '@Agent350', 9 => '@ARStrasser', 10 => '@johnzangas', 11 => '@350', 11 => '@bri_xy');
+
+//seed users to start crawling  from
+$users = array(1 => '@CamThompsonWNEW', 2 => '@Organizerx', 3 => '@Cool_Revolution', 4 => '@johnzangas', 5 => '@rousseau_ist', 6 => '@Gen_Knoxx', 7 => '@jamesFTinternet', 8 => '@Agent350', 9 => '@ARStrasser', 10 => '@johnzangas', 11 => '@350', 11 => '@bri_xy');
+
+//keywords to hunt for in the tweets.
 $keywords = array(1 => '?q=#KXLDissent', 2 => '?q=#RejectandProtect', 3 => '?q=#CowboyIndianAlliance', 4 => '?q=#nokxl', 5 => '?q=#OilSands', 6 => '?q=#KeystoneXL', 7 => '#NoKXL');
 
-$users = array(1 => 'Killpack');
-
-
-
+//this rate limit counter keeps track of number of get request to make sure we dont exceed  rate limit.
+$rate_limit_counter = 0;
 
 /*
  * add those user from the text of the collected tweets that have retweeted about
@@ -61,7 +63,7 @@ function addNewUsers($string) {
         if (preg_match_all($GLOBALS['pattern'], $string, $matches)) {
             foreach ($matches[0] as $k => $v) {
                 //getAllData($v);
-                $list_size = array_push($user_list, $v);
+                //$list_size = array_push($user_list, $v);
                 $GLOBALS['num_elements_in_new_user'] = array_push($GLOBALS['new_users'], $v);
             }
         }
@@ -75,17 +77,14 @@ function addNewUsers($string) {
 
 function processData($json_data, $user) {
 
-
     foreach ($json_data as $status) {
-        echo $status->created_at;
         $hashstring = $status->created_at;
 
         if ($status->id < $GLOBALS['max_id'] && !empty($status->id)) {
-            echo "Resetting the max_id to  ....." . $status->id . "\n";
             $GLOBALS['max_id'] = $status->id;
         }
 
-        $is_keyStone = true;
+        $is_keyStone = false;
         foreach ($GLOBALS['keywords'] as $keyword) {
             if (strpos($status->text, $keyword)) {
 
@@ -100,8 +99,8 @@ function processData($json_data, $user) {
                 downLoadImages($status, $hashValue);
             }
         }
-    }
-}
+}}
+
 
 function writeTweet($status, $hashValue) {
     echo "\n";
@@ -143,17 +142,7 @@ function makeRequests($get_field) {
 
 function getAllData($user) {
 
-
-    $first_getfield = '?screen_name=' . $user . '&count=20';
-    /*
-      // $first_getfield = '?screen_name=JamesFrancoTV&max_id=473257670443409408&count=200&include_rts=1';
-      $requestMethod = 'GET';
-      $twitter = new TwitterAPIExchange($GLOBALS['settings']);
-      $response = $twitter->setGetfield($first_getfield)->buildOauth($GLOBALS['url'], $requestMethod)->performRequest();
-      $json_data = json_decode($response);
-     */
-
-
+    $first_getfield = '?screen_name=' . $user . '&count=200';
     $json_data = makeRequests($first_getfield);
     if (!empty($json_data)) {
         echo "processing data for .." . $user . "\n";
@@ -163,23 +152,14 @@ function getAllData($user) {
      * getfield including the max_id for subsequent calls 
      */
     if ($GLOBALS['max_id'] != 0) {
-        $GLOBALS['new_getfield'] = '?screen_name=' . $user . '&count=20&max_id=' . $GLOBALS['max_id'];
+        $GLOBALS['new_getfield'] = '?screen_name=' . $user . '&count=200&max_id=' . $GLOBALS['max_id'];
     } else {
 
         echo "Empty Response! ";
     }
 
-
-
     while ($GLOBALS['old_max_id'] != $GLOBALS['new_max_id']) {
-        //  echo "Inside while loop!\n";
-        /* $requestMethod = 'GET';
-          $twitter = new TwitterAPIExchange($GLOBALS['settings']);
-          $response = $twitter->setGetfield($GLOBALS['new_getfield'])->buildOauth($GLOBALS['url'], $requestMethod)->performRequest();
-          $new_json_data = json_decode($response);
-         */
         $new_json_data = makeRequests($GLOBALS['new_getfield']);
-        print_r($new_json_data);
         $GLOBALS['old_max_id'] = $GLOBALS['new_max_id'];
 
         //get the next get_field
@@ -190,12 +170,12 @@ function getAllData($user) {
                 $GLOBALS['new_max_id'] = $status->id;
             }
         }
-        $GLOBALS['new_getfield'] = '?screen_name=' . $user . '&count=20&max_id=' . $GLOBALS['new_max_id'];
+        $GLOBALS['new_getfield'] = '?screen_name=' . $user . '&count=200&max_id=' . $GLOBALS['new_max_id'];
         //send this data to collect the media
         if (count((array) $new_json_data)) {
             processData($new_json_data, $user);
         }
-        echo "New_max_id ......" . $GLOBALS['new_max_id'] . "\n";
+       // echo "New_max_id ......" . $GLOBALS['new_max_id'] . "\n";
     }
 }
 
@@ -207,11 +187,11 @@ foreach ($users as $user) {
     }
 }
 
-echo "Number of tweets processed :" . $num_tweets . "\n";
-//from the list of all the users retweeting the concening tweets
-//foreach ($new_users as $user) {
-//    if (!in_array($user, $GLOBALS['processed_users'], $user)) {
-//        getAllData($user);
-//    }
-//}
 
+//from the list of all the users retweeting the concening tweets
+foreach ($new_users as $user) {
+    if (!in_array($user, $GLOBALS['processed_users'], $user)) {
+        getAllData($user);
+    }
+}
+echo "Number of tweets processed :" . $num_tweets . "\n";
